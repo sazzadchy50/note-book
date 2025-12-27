a58853d65bcdbc5af6fac3cabe060a87e2b656bd
sazzadchy50 <sazzadchy50@gmail.com>
2025-12-27 23:54:20 +0600

    lear how to use interface with generic and dynamic type declariation with generic and best practices

--- Typescript/2.4-Gereric_with_Interface.ts
+++ Typescript/2.4-Gereric_with_Interface.ts
@@ -1,20 +1,63 @@
 {
+  //Generic with interface
 
-type GenericArray<T>=Array<T>
- interface User{
-    name:string;
-    age:number
- }
-    
-const user:GenericArray<User> = [
-    {
-      name: "mezba",
-      age: 122,
-    },
-    {
-      name: "jonkar",
-      age: 100,
-    },
-  ];
+  interface Developer<T,X= null> {
+    name: string;
+    computer: {
+      brand: string;
+      model: string;
+      releaseYear: number;
+    };
+    smartWatch: T;
+    bike?: X
+  }
 
+  const poordeveloper: Developer<{
+    brand: string;
+    model: string;
+    display: string;
+  }> = {    //  Default parameter na thakle Argument a null ba undefined dite hobe 
+    name: "poor",
+    computer: {
+      brand: "walton",
+      model: "A1o",
+      releaseYear: 2020,
+    },
+    smartWatch: {
+      brand: "Emilab",
+      model: "Kw66",
+      display: "OLED",
+    },
+  };
+
+  type AppleWatch = {
+    //best practice for clean code
+    brand: string;
+    model: string;
+    heartTrack: boolean;
+    sleepTrack: boolean;
+  };
+  interface YamahaBike{
+    model: string;
+    engineCapacity: string;
+  }
+
+  const richdeveloper: Developer<AppleWatch, YamahaBike > = {
+    name: "Rich",
+    computer: {
+      brand: "Apple",
+      model: "A1",
+      releaseYear: 2022,
+    },
+    smartWatch: {
+      brand: "apple",
+      model: "AWatch",
+      heartTrack: true,
+      sleepTrack: true,
+    },
+    bike:{
+      model: "Yamaha",
+      engineCapacity: '160cc'
+    }
+  };
 }
