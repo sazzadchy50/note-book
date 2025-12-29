945c4d8b68c2ebde639833ae7c273e8e24c15136
sazzadchy50 <sazzadchy50@gmail.com>
2025-12-29 21:49:35 +0600

    working on

--- Typescript/2.6-Constraints_In_Typescript.ts
+++ Typescript/2.6-Constraints_In_Typescript.ts
@@ -1,22 +1,29 @@
 {
+  //constraints
+  //ex: constraints ortho force kora, kuno kichuke force korakei constaints bole
 
-    //constraints  
-     // constraints ortho force kora, kuno kichuke force korakei constaints bole
+  const addCourseToStudent = <T extends {id:number;name:string;email:string}>(student: T) => {  //ex: id,name,number gula oboshi thakte hobe,  extends kore constrints (baddo) koraikei bujay.  
+    const course = "Next Level web Development"; 
+    return {
+      ...student,
+      course,
+    };
+  };
 
-    const addCourseToStudent=<T>(student: T)=>{
-        const course ="Next Level web Development";
-        return{
-            ...student,
-            course
-        }
-    }
+  const student2 = addCourseToStudent({
+    amni: "amni",   // id, name,email chara hobe na
+  });
+  const student1 = addCourseToStudent<{
+    id: number;
+    name: string;
+    email: string;
+    devType: string;
+  }>({
+    id: 222,
+    name: "mr x",
+    email: "x@gmail.com",
+    devType: "NLWD",
+  });
 
-    const student1 = addCourseToStudent({
-        name:'mr x',
-        email:'x@gmail.com',
-        devType:'NLWD',
-    });
-
-    
-    //
+  //
 }
