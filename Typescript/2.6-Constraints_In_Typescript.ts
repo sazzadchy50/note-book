caadf7cd11e9e1548a1d33799179a7f5f4bedad6
sazzadchy50 <sazzadchy50@gmail.com>
2025-12-29 21:57:35 +0600

    2.6-Constraints_In_Typescripts: Uses a generic constraint to ensure the object contains the specified properties with the given types.

--- Typescript/2.6-Constraints_In_Typescript.ts
+++ Typescript/2.6-Constraints_In_Typescript.ts
@@ -2,8 +2,13 @@
   //constraints
   //ex: constraints ortho force kora, kuno kichuke force korakei constaints bole
 
-  const addCourseToStudent = <T extends {id:number;name:string;email:string}>(student: T) => {  //ex: id,name,number gula oboshi thakte hobe,  extends kore constrints (baddo) koraikei bujay.  
-    const course = "Next Level web Development"; 
+  const addCourseToStudent = <
+    T extends { id: number; name: string; email: string }
+  >(
+    student: T
+  ) => {
+    //ex: id,name,number gula oboshi thakte hobe,  extends kore constrints (baddo) koraikei bujay.
+    const course = "Next Level web Development";
     return {
       ...student,
       course,
@@ -11,7 +16,7 @@
   };
 
   const student2 = addCourseToStudent({
-    amni: "amni",   // id, name,email chara hobe na
+    amni: "amni", // id, name,email chara hobe na
   });
   const student1 = addCourseToStudent<{
     id: number;
